de7e3dfe92afd2f795f583f0d6fed74db362e865
vagrant <vagrant@homestead>
2019-12-09 23:36:10 +0000

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -3,13 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserAddress;
+use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
     public function index(Request $request)
     {
+        /*
+        var_dump($request->user()->addresses[0]);
+        exit(0);
+        */
+        //联想到User.php的addresses函数
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
+    }
+
+    public function create() {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
