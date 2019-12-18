cca4e30ca34f53938169b0ee055928dea383e174
vagrant <vagrant@homestead>
2019-12-18 02:56:45 +0000

    用户界面 - 使用优惠券下单

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,11 +9,12 @@ class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
-     *
+     *通常是用户触发的业务异常，不需要记录在日志里
      * @var array
      */
     protected $dontReport = [
         InvalidRequestException::class,
+        CouponCodeUnavailableException::class,
     ];
 
     /**
