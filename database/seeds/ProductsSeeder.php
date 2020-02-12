aeb6518b9f49125e7511fe37ce0dd1568451b10a
Git From Guangyao <guangyao.lao@isen.yncrea.fr>
2020-02-12 17:48:23 +0000

    no-use mdf

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+//use App\Models\Product;
 
 class ProductsSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class ProductsSeeder extends Seeder
     {
         // 创建 30 个商品
         $products = factory(\App\Models\Product::class, 30)->create();
+        //$products = factory(Product::class, 30)->create();
         foreach ($products as $product) {
             // 创建 3 个 SKU，并且每个 SKU 的 `product_id` 字段都设为当前循环的商品 id
             $skus = factory(\App\Models\ProductSku::class, 3)->create(['product_id' => $product->id]);
